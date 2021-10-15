66f910eaa45385db3f2545403c48642af98fdd3f
Arushi Agarwal <arushiagarwal@Arushis-MacBook-Pro.local>
2021-10-15 16:38:52 -0400

    a5 build part done

---
+++ public/a5/twitter/explore/PostList/posts.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    "topic": "Web Development",
+    "userName": "ReactJS",
+    "time": "2h",
+    "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+    "content": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+    "image": "https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+  },
+  {
+    "topic": "",
+    "userName": "JavaScript",
+    "time": "1 day",
+    "title": "JavaScript is programming language that can run on browsers as well as desktops",
+    "image": "https://cdn.worldvectorlogo.com/logos/logo-javascript.svg",
+    "tweets": "123K"
+  },
+  {
+    "topic": "Web Development",
+    "userName": "",
+    "title": "jQuery",
+    "time": "last week",
+    "image": "https://e7.pngegg.com/pngimages/911/765/png-clipart-jquery-computer-icons-javascript-jquery-logo-logo-jquery-ui.png",
+    "tweets": "122K"
+  },
+  {
+    "topic": "Web Development",
+    "userName": "NodeJS",
+    "title": "",
+    "time": "last month",
+    "image": "https://png.pngitem.com/pimgs/s/168-1680234_nodejs-logo-svg-hd-png-download.png",
+    "tweets": "120K"
+  }
+];
---
+++ public/a5/twitter/explore/WhoToFollowList/who.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        avatarIcon: 'https://1000logos.net/wp-content/uploads/2020/09/Java-Emblem.jpg',
+        userName: 'Java',
+        handle: 'Java',
+    },
+    {
+        avatarIcon: 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/j3dbh6kok0dket3uz2vy',
+        userName: 'Relativity Space',
+        handle: 'relativityspace',
+    },
+    {
+        avatarIcon: 'https://www.c-r-y.org.uk/wp-content/uploads/2016/10/Virgin-logo.png',
+        userName: 'Virgin Galactic',
+        handle: 'virgingalactic',
+    },
+    {
+        avatarIcon: 'https://lh3.googleusercontent.com/proxy/EpdB4_Ewhs4NA5CEVtHIOJNJnjpAIw1rpPitJ5-dGRr4o787PcFSDfe18OiG3dw17PEFiHXCtd4zaQhT4mca5VAU425UNcw',
+        userName: 'NASA',
+        handle: 'NASA',
+    },
+    {
+        avatarIcon: 'https://storage.googleapis.com/webdesignledger.pub.network/WDL/12f213e1-t1.jpg',
+        userName: 'Tesla',
+        handle: 'Tesla',
+    },
+];
